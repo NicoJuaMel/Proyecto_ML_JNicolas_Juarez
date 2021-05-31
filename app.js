b149dac22de3550b04ef9d8d6ec28649463ae12f
NicoJuaMel <nicojuamel@hotmail.com>
2021-05-31 19:01:57 -0300

    listo para heroku

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const path = require("path");
 const app = express();
 app.use(express.static(path.resolve(__dirname,"./public")));
-app.listen(3050, ()=>console.log("Ok Server start http://localhost:3050"));
+app.listen(process.env.PORT || 3050, ()=>console.log("Ok Server start http://localhost:3050"));
+
+
 app.get("/",function(req,res){
     res.sendFile(path.join(__dirname,"/views/home.html"))
 });

1dbc9311ea6cd9255ed8a0f4b8ea7dc08da2c34d
NicoJuaMel <nicojuamel@hotmail.com>
2021-05-31 18:49:07 -0300

    Primer commit MercadoLiebre

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.static(path.resolve(__dirname,"./public")));
+app.listen(3050, ()=>console.log("Ok Server start http://localhost:3050"));
+app.get("/",function(req,res){
+    res.sendFile(path.join(__dirname,"/views/home.html"))
+});
+app.get("/register.html",function(req,res){
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+});
+app.get("/login.html",function(req,res){
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+});

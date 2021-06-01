d3d646647871dfe4bf4ece6898e85b4a2e3f316c
NicoJuaMel <nicojuamel@hotmail.com>
2021-05-31 21:13:11 -0300

    heroku

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const fs = require("fs");
 const path = require("path");
 const app = express();
 app.use(express.static(path.resolve(__dirname,"./public")));
 
-app.listen(process.env.PORT||3050, function(){
+app.set("port",process.env.PORT||3050)
+
+app.listen(app.get("port"), function(){
     console.log("Ok Server start http://localhost:3050");
 })
-
 
 app.get("/",function(req,res){
     res.sendFile(path.join(__dirname,"/views/home.html"))
